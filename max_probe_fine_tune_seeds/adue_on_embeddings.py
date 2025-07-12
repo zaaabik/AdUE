ac0c799eee9faf8be60bc9c207e6c6685b038797
Zabolotnyi_Artem <zaaabik@gmail.com>
2025-07-12 20:02:34 +0300

    code to produce result on pre-computed embeddings

--- max_probe_fine_tune_seeds/adue_on_embeddings.py
+++ max_probe_fine_tune_seeds/adue_on_embeddings.py
@@ -47,7 +47,7 @@ def train(cfg):
     best_run, all_runs = search_hyperparameters(**state)
 
     current_output_dir = os.path.join(cfg.save_dir, cfg.data.name)
-    os.makedirs(cfg.save_dir, exist_ok=True)
+    os.makedirs(current_output_dir, exist_ok=True)
     with open(os.path.join(current_output_dir, 'best_run.pickle'), 'wb') as f:
         pickle.dump(best_run, f)
 
