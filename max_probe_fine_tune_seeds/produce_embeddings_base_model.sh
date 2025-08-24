1c69f943cb5751cee0c8bd14ba0ae24aaf120f2a
Zabolotnyi_Artem <zaaabik@gmail.com>
2025-08-25 00:46:42 +0300

    remove epoch from hparams search

--- max_probe_fine_tune_seeds/produce_embeddings_base_model.sh
+++ max_probe_fine_tune_seeds/produce_embeddings_base_model.sh
@@ -1,1 +1,8 @@
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/produce_embeddings.py train_on_dataset=train experiment=llama_chat_base grid=debug data.name=MMLU
+
+
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/produce_embeddings.py \
+train_on_dataset=train experiment=llama_chat_base data.llm_batch_size=1 \
+grid=debug data.name=MMLU ++data.dataset.n_shot=5 \
+++data.dataset.debug=false data.max_length=4096
+
