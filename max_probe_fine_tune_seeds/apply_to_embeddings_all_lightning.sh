8a0989f550ea7014fe1b033d31c87d8003eaeca1
Zabolotnyi_Artem <zaaabik@gmail.com>
2025-08-30 13:15:16 +0300

    update eval command

--- max_probe_fine_tune_seeds/apply_to_embeddings_all_lightning.sh
+++ max_probe_fine_tune_seeds/apply_to_embeddings_all_lightning.sh
@@ -1,11 +1,11 @@
-export grid_name=entropyft_wo_lr,entropyft_ablation_bce_only
-OMP_NUM_THREADS=2 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
+export grid_name=entropyft,entropyft_ablation_bce_only
+OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
 experiment=llama_chat_base grid=${grid_name} data.name=MMLU seed=0 \
 embedding_path='${oc.env:PROJECT_ROOT}/data/embeddings/llama_chat_results_ds_0_train_gird_full/state.pkl'
 
 
 export grid_name=entropyft_ablation_bce_only_linear_random,entropyft_l2sp,entropyft_reg
-OMP_NUM_THREADS=2 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
+OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
 experiment=llama_chat_base grid=${grid_name} data.name=MMLU seed=0 \
 embedding_path='${oc.env:PROJECT_ROOT}/data/embeddings/llama_chat_results_ds_0_train_gird_full/state.pkl'
 
@@ -41,12 +41,12 @@ OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.
 OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train experiment=llama grid=${grid_name} data.name=SST5,SST2,cola,toxigen,20newsgroups seed=4,3 &
 
 export grid_name=srft_ablation_bce_only_linear_random,srft_l2sp,srft_reg
-OMP_NUM_THREADS=2 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
+OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
 experiment=llama_chat_base grid=${grid_name} data.name=MMLU seed=0 \
 embedding_path='${oc.env:PROJECT_ROOT}/data/embeddings/llama_chat_results_ds_0_train_gird_full/state.pkl'
 
-export grid_name=srft_wo_lr,srft_ablation_bce_only
-OMP_NUM_THREADS=2 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
+export grid_name=srft,srft_ablation_bce_only
+OMP_NUM_THREADS=4 python max_probe_fine_tune_seeds/adue_on_embeddings_lightning.py -m train_on_dataset=train \
 experiment=llama_chat_base grid=${grid_name} data.name=MMLU seed=0 \
 embedding_path='${oc.env:PROJECT_ROOT}/data/embeddings/llama_chat_results_ds_0_train_gird_full/state.pkl'
 
